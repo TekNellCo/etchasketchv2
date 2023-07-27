b2772e91ebf58a280eeb20d75fa2cd7fa24fca67
NellyNell <129366078+TekNellCo@users.noreply.github.com>
2023-07-26 23:01:11 -0400

    add reset and colors. need to figure input slider?remove then add divs on change?

--- script.js
+++ script.js
@@ -1,21 +1,46 @@
 const drawingarea = document.querySelector('.drawingarea');
-window.onload = populateDiv();
+let input = document.querySelector('input');
+let dimensions = 16;
+window.onload = populateDiv(dimensions);
 let divfill = document.querySelectorAll('.divfill');
+let buttonColor = document.querySelectorAll('.colorbutton');
+// let reset = document.querySelector('.reset')
+let color = "black"
+let reset = document.querySelector('.reset')
+reset.addEventListener('click',clear)
 
 
+// input.addEventListener("change",()=>{
+//     populateDiv(input.value);
+// })a
 
-divfill.forEach((div)=>{
-    div.addEventListener("mouseover",()=>{
-        div.style.cssText= "background-color: black";
+function clear(){
+    divfill.forEach((div)=>{
+        div.style.cssText = "background-color : white"
+    })
+}
+
+function populateDiv(a){
+
+    for(i=0; i< a*a; ++i){
+    const drawingareadivs = document.createElement('div');
+    drawingareadivs.classList.add('divfill');
+    drawingarea.append(drawingareadivs);
+    drawingarea.style.cssText = `grid-template-rows:repeat(${a},1fr); grid-template-columns: repeat(${a},1fr)`;
+    }
+}
+
+buttonColor.forEach((btn)=>{
+    btn.addEventListener('click',()=>{
+        return color = btn.innerText
     })
 })
 
-function populateDiv(){
-    for(i=0; i< 16*16; ++i){
-    const drawingareadivs = document.createElement('div');
-    drawingareadivs.classList.add('divfill')
-    drawingarea.append(drawingareadivs)
-    }
-}
+divfill.forEach((div)=>{
+    div.addEventListener("mouseover",()=>{
+        div.style.cssText= `background-color: ${color}`;
+    })
+})
+
 
 
