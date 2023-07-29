2e9de3e44cb7fd6433198a77b46fe45986fb90d4
NellyNell <129366078+TekNellCo@users.noreply.github.com>
2023-07-28 23:30:26 -0400

    add random button

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 const drawingarea = document.querySelector('.drawingarea');
 let input = document.querySelector('input');
-populateDiv(input.value)
-let divfill = document.querySelectorAll('.divfill');
+populateDiv(input.value);
 let buttonColor = document.querySelectorAll('.colorbutton');
-let color = "black"
-let reset = document.querySelector('.reset')
-reset.addEventListener('click',clearGrid)
-input.addEventListener("change",clearGrid)
+let color = "black";
+let reset = document.querySelector('.reset');
+reset.addEventListener('click',clearGrid);
+input.addEventListener("change",clearGrid);
+let random = document.querySelector('.random');
+
+
+random.addEventListener('click',()=>{
+    return color = `hsl(${Math.floor(Math.random()* 360)},${Math.floor(Math.random()* 100)}%,${Math.floor(Math.random()* 100)}%)`
+
+})
+
+
+
 
 function clearGrid(){
     drawingarea.innerHTML = "";
@@ -16,7 +25,6 @@ function clearGrid(){
 function populateDiv(a){
     for(i=0; i< a*a; ++i){
     const drawingareadivs = document.createElement('div');
-    drawingareadivs.classList.add('divfill');
     drawingarea.append(drawingareadivs);
     drawingarea.style.cssText = `grid-template-rows:repeat(${a},1fr); grid-template-columns: repeat(${a},1fr)`;
     drawingareadivs.addEventListener("mouseover",()=>{
